3cee43862c19ced8d9e55502408bda8bc03d024a
Uzayr Sonday <uzayr.sonday@gmail.com>
2025-01-17 10:02:02 +0200

    bug locations

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -12,4 +12,5 @@ export const config = {
   ],
 };
 
-
+console.log("CLERK_SECRET_KEY:", process.env.CLERK_SECRET_KEY);
+console.log("NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
