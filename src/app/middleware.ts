7502555916bb8f334d114f1dd3bb09bf1d2903ad
Uzayr Sonday <uzayr.sonday@gmail.com>
2025-01-17 10:23:22 +0200

    clerk fix

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
+  publicRoutes: [
     "/((?!.+\\.[\\w]+$|_next).*)", // Match all routes except static files and Next.js internals
     "/", 
     "/(api|trpc)(.*)",
@@ -11,6 +11,7 @@ export const config = {
     "/sign-up",
   ],
 };
+
 
 console.log("CLERK_SECRET_KEY:", process.env.CLERK_SECRET_KEY);
 console.log("NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
