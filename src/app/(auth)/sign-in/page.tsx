ba2714ecfc4d801447d0b224f5e1fcb9dc11fb36
Uzayr Sonday <uzayr.sonday@gmail.com>
2025-01-17 13:37:05 +0200

    signs

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+// src/app/(auth)/sign-in/page.tsx
+export default function SignInPage() {
+    return (
+      <div>
+        <h1>Sign In</h1>
+        <p>Please sign in to continue.</p>
+        {/* Add your Clerk sign-in component here */}
+      </div>
+    );
+  }
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+// src/app/(auth)/sign-up/page.tsx
+export default function SignUpPage() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <p>Create an account to get started.</p>
+        {/* Add your Clerk sign-up component here */}
+      </div>
+    );
+  }
